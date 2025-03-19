3d0f8512064075a252b1fc2b708e307973870a9d
vitorraiaa <vitortraia@gmail.com>
2025-03-19 11:06:11 -0300

    fix

--- src/main/java/br/insper/prova/ProvaApplication.java
+++ src/main/java/br/insper/prova/ProvaApplication.java
@@ -1,4 +1,13 @@
 package br.insper.prova;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaApplication.class, args);
+    }
+
 }
