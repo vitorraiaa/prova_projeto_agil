601549b2db1a2932fa866de7000a24912951b31b
vitorraiaa <vitortraia@gmail.com>
2025-03-19 10:16:13 -0300

    base

---
+++ src/test/java/br/insper/produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Se o contexto carregar, o teste passa.
+    }
+}
